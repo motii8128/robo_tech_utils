981081d7e587257fca3ffc9797b534bdb5c323bc
motii <108280115+motii8128@users.noreply.github.com>
2023-10-21 15:51:03 +0900

    add quaternion converter

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,24 @@ pub fn identity_matrix()->na::Matrix3<f64>
 
     i
 }
+
+pub fn euler_to_quaternion(euler:na::Vector3<f64>)->na::Vector4<f64>
+{
+    let cos_a = (euler.x / 2.0).cos();
+    let sin_a = (euler.x / 2.0).sin();
+
+    let cos_b = (euler.y / 2.0).cos();
+    let sin_b = (euler.y / 2.0).sin();
+
+    let cos_r = (euler.z / 2.0).cos();
+    let sin_r = (euler.z / 2.0).sin();
+
+    let quaternion = na::Vector4::<f64>::new(
+        cos_a*cos_b*cos_r - sin_a*sin_b*sin_r,
+        sin_a*cos_b*cos_r + cos_a*sin_b*sin_r,
+        cos_a*sin_b*cos_r - sin_a*cos_b*sin_r,
+        cos_a*cos_b*sin_r + sin_a*sin_b*cos_r,
+    );
+
+    quaternion
+}
