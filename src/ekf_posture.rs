c5d9900a24baad499208b870fc319a99271f14ff
motii <108280115+motii8128@users.noreply.github.com>
2023-10-21 14:18:01 +0900

    Add files via upload

--- src/ekf_posture.rs
+++ src/ekf_posture.rs
@@ -1,6 +1,6 @@
 extern crate nalgebra as na;
 
-pub fn x_init()->na::Vector3<f64>
+pub fn vector3init()->na::Vector3<f64>
 {
     let init = na::Vector3::<f64>::new(
         0.0,
@@ -11,9 +11,9 @@ pub fn x_init()->na::Vector3<f64>
     init
 }
 
-pub fn p_init(dt:f64)->na::Matrix3<f64>
+pub fn p_init(delta_t:f64)->na::Matrix3<f64>
 {
-    let dt_pow = dt.powi(2);
+    let dt_pow = delta_t.powi(2);
     let init = na::Matrix3::<f64>::new(
         0.0174*dt_pow, 0.0, 0.0,
         0.0, 0.0174*dt_pow, 0.0,
@@ -23,12 +23,12 @@ pub fn p_init(dt:f64)->na::Matrix3<f64>
     init
 }
 
-pub fn calc_input(gyro:na::Vector3<f64>, dt:f64)->na::Vector3<f64>
+pub fn calc_input(gyro:na::Vector3<f64>, delta_t:f64)->na::Vector3<f64>
 {
     let u = na::Vector3::<f64>::new(
-        gyro.x * dt,
-        gyro.y * dt,
-        gyro.z * dt,
+        gyro.x * delta_t,
+        gyro.y * delta_t,
+        gyro.z * delta_t,
     );
 
     u
@@ -36,10 +36,37 @@ pub fn calc_input(gyro:na::Vector3<f64>, dt:f64)->na::Vector3<f64>
 
 pub fn calc_observe(accel:na::Vector3<f64>)->na::Vector2<f64>
 {
+    let calc_x_comp = accel.y / accel.z;
+    let calc_y_comp = (-1.0*accel.x) / (((accel.y.powi(2))+(accel.z.powi(2))).sqrt());
+
     let z = na::Vector2::<f64>::new(
-        (accel.y / accel.z).atan(),
-        (-1.0*accel.x)/((accel.y.powi(2) + accel.z.powi(2)).sqrt())
+        calc_x_comp.atan(),
+        calc_y_comp,
     );
 
     z
+}
+
+pub fn get_gyro_noise(delta_t:f64)->na::Matrix3<f64>
+{
+    let diagonal_comp = 0.0174* (delta_t.powi(2));
+    let q = na::Matrix3::<f64>::new(
+        diagonal_comp, 0.0, 0.0,
+        0.0, diagonal_comp, 0.0,
+        0.0, 0.0, diagonal_comp
+    );
+
+    q
+}
+
+pub fn get_accel_noise(delta_t:f64)->na::Matrix2<f64>
+{
+    let diagonal_comp = delta_t.powi(2);
+
+    let r = na::Matrix2::<f64>::new(
+        diagonal_comp, 0.0,
+        0.0, diagonal_comp
+    );
+
+    r
 }
