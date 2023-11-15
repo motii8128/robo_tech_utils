0a4545bbd604741b9293850982c8dbb9fb3ea9ef
motii <108280115+motii8128@users.noreply.github.com>
2023-11-15 12:10:58 +0900

    Add files via upload

--- src/localization/obstacle_scan.rs
+++ src/localization/obstacle_scan.rs
@@ -1,6 +1,6 @@
 use safe_drive::msg::common_interfaces::sensor_msgs;
 
-pub fn get_nearest_front(laser_scan:sensor_msgs::msg::LaserScan)->f32
+pub fn get_front_distance(laser_scan:sensor_msgs::msg::LaserScan)->f32
 {
     let ranges = laser_scan.ranges.as_slice();
 
@@ -11,4 +11,23 @@ pub fn get_nearest_front(laser_scan:sensor_msgs::msg::LaserScan)->f32
     let result = ranges.get(get_num).unwrap();
 
     *result
+}
+
+pub fn get_nearest(laser_scan:sensor_msgs::msg::LaserScan)->f32
+{
+    let ranges = laser_scan.ranges.as_slice();
+
+    let array_len = ranges.len();
+
+    let mut min = *ranges.get(0).unwrap();
+
+    for u in 1..array_len
+    {
+        if min > *ranges.get(u).unwrap()
+        {
+            min = *ranges.get(u).unwrap();
+        }
+    }
+
+    min
 }
